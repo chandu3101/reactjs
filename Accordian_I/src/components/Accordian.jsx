61c80f2bb66837c249bb40b0dec6ad1293f0d5a0
Kumar Chandan <chandan.kc3101@gmail.com>
2023-12-25 18:08:56 +0530

    complete with accordion

--- Accordian_I/src/components/Accordian.jsx
+++ Accordian_I/src/components/Accordian.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
-function Accordian(){
-    return(
+function Accordian() {
+
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelect, setMultiSelect] = useState(false);
+    const [arrMultiSelect, setArrMultiSelect] = useState([]);
+
+    function sigleSelection(item) {
+        setSelected(item === selected ? null : item);
+    }
+    function multiSelectionHandle(item) {
+        var copyArrMyltiSelect = [...arrMultiSelect];
+        var findIndexofCurrentID = copyArrMyltiSelect.indexOf(item);
+
+        if (findIndexofCurrentID == -1)
+            copyArrMyltiSelect.push(item);
+        else
+            copyArrMyltiSelect.splice(findIndexofCurrentID, 1);
+
+        setArrMultiSelect(copyArrMyltiSelect);
+        console.log(arrMultiSelect, copyArrMyltiSelect, arrMultiSelect);
+
+    }
+
+    return (
         <>
-        <h1>Accordian</h1>
+            <div className="wrapper">
+                <button onClick={() => setMultiSelect(!enableMultiSelect)}>Enable Multi Select Accordion</button>
+                <div className="accordion">
+                    {
+                        data && data.length > 0 ?
+                            data.map(dataItem => <div className="item">
+                                <div onClick={enableMultiSelect ?
+                                    () => multiSelectionHandle(dataItem.id)
+                                    : () => sigleSelection(dataItem.id)
+                                } className="title">
+                                    <h3>{dataItem.question}</h3>
+                                    <span>+</span>
+                                    {
+                                        enableMultiSelect ?
+                                            arrMultiSelect.indexOf(dataItem.id) !== -1 &&
+                                            <div className="">{dataItem.answer}</div>
+
+                                            : selected === dataItem.id &&
+                                            <div className="">{dataItem.answer}</div>
+
+                                    }
+                                    {/* {
+                                        selected === dataItem.id ?
+                                            <div className="">{dataItem.answer}</div>
+                                            : null
+                                    } */}
+                                </div>
+                            </div>)
+                            : <div> No Data Found</div>
+                    }
+                </div>
+            </div >
         </>
     )
 }
---
+++ Accordian_I/src/components/data.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        id : '1',
+        question : 'What is your name?',
+        answer : 'My name is React'
+    },
+    
+    {
+        id : '2',
+        question : 'What is your Father name?',
+        answer : 'My name is JavaScritp'
+    },
+    {
+        id : '3',
+        question : 'What is your mother name?',
+        answer : 'My name is jQuery'
+    }
+]
+
+
+export default data;
