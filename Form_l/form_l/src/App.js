567947fe090d6b4819690144c04d63d4813fb149
chandu3101 <chandan.kc3101@gmail.com>
2024-04-21 17:46:34 +0530

    created form

--- Form_l/form_l/src/App.js
+++ Form_l/form_l/src/App.js
@@ -1,24 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { createElement } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Employee Details"),
+    React.createElement(
+      "form", {},
+      React.createElement(
+        "div", { className: "firstName" },
+        React.createElement(
+          "label", { className: "tagName" ,for:"fName"}, "First Name : "
+        ),
+
+        React.createElement(
+          "input", { className: "inputForm",  id:"fName" },
+        ),
+      ),
+      React.createElement(
+        "div", { className: "lastName" },
+
+        React.createElement(
+          "label", { className: "tagName", for:"lName" }, "Last Name : "
+        ),
+        React.createElement(
+          "input", { className: "inputForm", id:"lName" },
+        )
+      ),
+      React.createElement(
+        "div", { className: "chooseGender" },
+       
+        React.createElement(
+          "input", { className: "inputForm",type:"radio", id:"male", value:"m", name:"abx" },
+        ),
+        React.createElement("label",{className: "tagName", for:"male"},"Male"),
+        React.createElement(
+          "input", { className: "inputForm",type:"radio", id:"female", value:"f", name:"abx" },
+        ),
+        React.createElement("label",{className: "tagName", for:"female"},"Female"),
+      ),
+
+      React.createElement(
+        "div", {className : "empID"},
+        React.createElement(
+          "label", {className:"tagName", for:"emp"},"Employee ID : "
+        ),
+        React.createElement(
+          "input", {className:"inputForm", type:"text", id:"emp"}
+        ),
+      ),
+
+      React.createElement(
+        "div", {className:"designation"},
+        React.createElement(
+          "label",{className:"tagName", for:"desig"}, "Designation : ",
+          React.createElement(
+            "input", {className:"inputForm", id:"desig"}
+          ),
+        )
+      ),
+
+      React.createElement(
+        "div",{className:"empPhone"},
+        React.createElement(
+          "label", {className:"tagName", for:"pno"},"Phone No : ",
+          React.createElement(
+            "input", {className : "inputForm", id:"pno"}
+          )
+        )
+      ),
+
+      React.createElement(
+        "div", {className:"btnSubmit"},
+        React.createElement(
+          "button", {className:"submit"}, "Save"
+        )
+      )
+    )
+
+  
   );
 }
 
